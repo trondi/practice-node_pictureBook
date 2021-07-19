de89ed4e584cb8ca8f21f45de1ac0d0f2ac9926b
ericabyss <erica051746@naver.com>
2021-07-19 17:28:56 +0900

    7-2_

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
-//const fs = require('fs');
-//const path = require('path');
+const path = require('path');
 
-console.log(process.argv); //인자를 받아옴
+const folder = process.argv[2]; //인자를 받아옴
 
-//if()
-//fs.rename(oldpath, newpath, callback);
+if(!folder) { //만약 사용자가 적절한 폴더를 가져오지 못했다면 에러 출력
+    console.error('Please enter folder name in Pictures');
+}
+
+const workingDir = path.join(os.homedir(), 'Pictures', folder);
+//현재 운영체제에 있는 워킹 디렉토리에 우리가 원하는 픽처 폴더안에 사용자가 원하는 폴더에서 작업
+console.log(workingDir);
